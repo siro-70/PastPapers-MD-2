ea4daa4e83cb5a0ec2538a6201c116f43938ace5
SLRealTech <91659400+slrealtech@users.noreply.github.com>
2022-07-19 19:00:47 +0930

    Update pastpaper.js

--- pastpaper.js
+++ pastpaper.js
@@ -17,7 +17,6 @@ const { Primbon } = require('scrape-primbon')
 const primbon = new Primbon()
 const { smsg, formatp, tanggal, formatDate, getTime, isUrl, sleep, clockString, runtime, fetchJson, getBuffer, jsonformat, format, parseMention, getRandom } = require('./lib/myfunc')
 const nexusnw = require('xfarr-api')
-const Language = require('./language')
 
 //rpg function\\
    const { 
